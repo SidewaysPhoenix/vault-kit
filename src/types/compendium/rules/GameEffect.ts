76704666cc491332b5d50309400c004208ca768f
SidewaysPhoenix <madams2550@gmail.com>
2026-08-24 15:24:14 -0500

    Added optional field for effect name to gameEffect

--- src/types/compendium/rules/GameEffect.ts
+++ src/types/compendium/rules/GameEffect.ts
@@ -1,4 +1,5 @@
 export type GameEffect = {
+  effectName?: string; //Name of the effect, e.g. "Sealed Enviroment", "Enhanced Strength", etc.
   target: string; //Effect Mechanic to target, e.g. "health", "strength", "intelligence", etc.
   operation: "add" | "subtract" | "set" | "gain" | "remove";
   value?: number | string;
