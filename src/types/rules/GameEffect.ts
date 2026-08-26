1051278c56b14165b973866d396c2a18f6d830fc
SidewaysPhoenix <madams2550@gmail.com>
2026-08-25 21:51:00 -0500

    Added ability for gameEffect to have proper targets and ranges if area is used. Created gameStat so that stats can be targeted for manipulation.

--- src/types/rules/GameEffect.ts
+++ src/types/rules/GameEffect.ts
@@ -1,13 +1,19 @@
-import { Triggerable } from "./Trigger";
+import type { Triggerable } from "./Trigger";
 
 
 export type GameEffect = {
   effectName?: string; //Name of the effect, e.g. "Sealed Enviroment", "Enhanced Strength", etc.
-  target: string; //Effect Mechanic to target, e.g. "health", "strength", "intelligence", etc.
-  operation: "add" | "subtract" | "set" | "gain" | "remove";
+  description: string;
+  target: "self" | "actionTarget" | "triggerSource" | "area";
+  range?: "reach" | "close" | "medium" | "long" | "extreme" | "unlimited"; //Used if area is selected as the target, e.g. "reach", "close", "medium", "long", "extreme", or "unlimited"
+  rangeFilter?: "allies" | "enemies" | "all"; //Filter for the range, e.g. "allies", "enemies", or "all"
+  
+  gameStat?: string; //Game stat to modify, e.g. "strength", "dexterity", "intelligence", etc.
+  operation?: "add" | "subtract" | "set" | "gain" | "remove";
   value?: number | string;
+  
   durationUnit?: "round" | "scene";
   durationLength?: number;
-  description: string;
+  
 
 } & Triggerable; //Trigger for the effect, e.g. "onHit", "onUse", "onEquip", "effectRolled", etc.
